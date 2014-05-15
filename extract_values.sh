1f16ff1cef70d51b9fccb3dba88a9dcfcef5e972
Damien <contact@dvilleneuve.fr>
2014-05-15 21:53:48 +0200

    Add a script to fetch all icon values

---
+++ extract_values.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+URL="https://fortawesome.github.io/Font-Awesome/cheatsheet/"
+PATTERN_VALUE="&#x([0-9a-zA-Z]+)"
+PATTERN_KEY="(fa-[a-zA-Z0-9_-]+)"
+
+curl -silent "$URL" | sed -En "
+	/$PATTERN_VALUE/ {
+		N
+		/$PATTERN_KEY/ {
+			s/.*$PATTERN_VALUE.*\n.*$PATTERN_KEY.*/\2('\\\u\1'),/
+			s/-/_/g
+			p
+		}
+	}"
