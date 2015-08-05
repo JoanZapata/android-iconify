7f10b38fb5c21fe04eb4c528d719795be98117cb
Joan Zapata <zapata.joan@gmail.com>
2015-08-05 14:54:35 +0200

    Supported Typicons

--- android-iconify-sample/src/main/java/com/joanzapata/android/iconify/sample/SampleApplication.java
+++ android-iconify-sample/src/main/java/com/joanzapata/android/iconify/sample/SampleApplication.java
@@ -3,12 +3,14 @@ package com.joanzapata.android.iconify.sample;
 import android.app.Application;
 import com.joanzapata.android.iconify.Iconify;
 import com.joanzapata.android.iconify.fonts.FontAwesomeModule;
+import com.joanzapata.android.iconify.fonts.TypiconsModule;
 
 public class SampleApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Iconify.with(new FontAwesomeModule());
+        Iconify.with(new FontAwesomeModule())
+                .with(new TypiconsModule());
     }
 }
