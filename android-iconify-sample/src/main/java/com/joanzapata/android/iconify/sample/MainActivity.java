8a93ddbe276a7820ff11d3d892be9cbd022d0872
Joan Zapata <zapata.joan@gmail.com>
2015-08-07 02:09:28 +0200

    Improved sample activity with collapsing toolbar layout

--- android-iconify-sample/src/main/java/com/joanzapata/android/iconify/sample/MainActivity.java
+++ android-iconify-sample/src/main/java/com/joanzapata/android/iconify/sample/MainActivity.java
@@ -1,13 +1,39 @@
 package com.joanzapata.android.iconify.sample;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import com.joanzapata.android.iconify.fonts.FontAwesomeIcons;
+import com.joanzapata.android.iconify.sample.utils.AndroidUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.tabs) TabLayout tabLayout;
+    @Bind(R.id.recyclerView) RecyclerView recyclerView;
+    @Bind(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // Setup toolbar
+        setSupportActionBar(toolbar);
+
+        // Tab layout
+        tabLayout.addTab(tabLayout.newTab().setText("FontAwesome"));
+        tabLayout.addTab(tabLayout.newTab().setText("Typicons"));
+
+        // Fill recycler view
+        recyclerView.setHasFixedSize(true);
+        int nbColumns = AndroidUtils.getScreenSize(this).width / getResources().getDimensionPixelSize(R.dimen.item_width);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, nbColumns));
+        recyclerView.setAdapter(new IconAdapter(FontAwesomeIcons.values()));
     }
 }
---
+++ android-iconify-sample/src/main/java/com/joanzapata/android/iconify/sample/utils/AndroidUtils.java
@@ -0,0 +1,31 @@
+package com.joanzapata.android.iconify.sample.utils;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.WindowManager;
+
+public final class AndroidUtils {
+
+    // Prevent instantiation
+    private AndroidUtils() {}
+
+    /** Returns the available screensize, including status bar and navigation bar */
+    public static Size getScreenSize(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point out = new Point();
+        display.getSize(out);
+        return new Size(out.x, out.y);
+    }
+
+    public static class Size {
+        public final int width;
+        public final int height;
+
+        public Size(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
