6f7c129cf31c63d8a123a4850ba8b020a7ecc9e1
Joan Zapata <zapata.joan@gmail.com>
2015-08-09 21:22:00 +0200

    Implemented #39 Set font size of icon different from text

--- android-iconify/src/main/java/com/joanzapata/iconify/internal/CustomTypefaceSpan.java
+++ android-iconify/src/main/java/com/joanzapata/iconify/internal/CustomTypefaceSpan.java
@@ -6,26 +6,32 @@ import android.text.TextPaint;
 import android.text.style.TypefaceSpan;
 
 public class CustomTypefaceSpan extends TypefaceSpan {
-    private final Typeface newType;
+    private final Typeface type;
+    private final float iconSizePx;
+    private final int iconColor;
 
-    public CustomTypefaceSpan(String family, Typeface type) {
+    public CustomTypefaceSpan(String family, Typeface type, float iconSizePx, int iconColor) {
         super(family);
-        newType = type;
+        this.type = type;
+        this.iconSizePx = iconSizePx;
+        this.iconColor = iconColor;
     }
 
     @Override
     public void updateDrawState(TextPaint ds) {
-        applyCustomTypeFace(ds, newType);
+        applyCustomTypeFace(ds, type);
     }
 
     @Override
     public void updateMeasureState(TextPaint paint) {
-        applyCustomTypeFace(paint, newType);
+        applyCustomTypeFace(paint, type);
     }
 
     private void applyCustomTypeFace(Paint paint, Typeface tf) {
         paint.setFakeBoldText(false);
         paint.setTextSkewX(0f);
         paint.setTypeface(tf);
+        if (iconSizePx > 0) paint.setTextSize(iconSizePx);
+        if (iconColor > 0) paint.setColor(iconColor);
     }
 }
--- android-iconify/src/main/java/com/joanzapata/iconify/internal/ParsingUtil.java
+++ android-iconify/src/main/java/com/joanzapata/iconify/internal/ParsingUtil.java
@@ -1,9 +1,11 @@
 package com.joanzapata.iconify.internal;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.util.SparseArray;
+import android.util.TypedValue;
 import com.joanzapata.iconify.Icon;
 
 import java.util.List;
@@ -13,40 +15,50 @@ public final class ParsingUtil {
     // Prevents instantiation
     private ParsingUtil() {}
 
-    public static SpannableString parse(Context context, List<IconFontDescriptorWrapper> iconFontDescriptors, CharSequence text) {
+    public static SpannableString parse(
+            Context context,
+            List<IconFontDescriptorWrapper> iconFontDescriptors,
+            CharSequence text) {
         context = context.getApplicationContext();
 
         // Analyse the text and replace {} blocks with the appropriate character
         // Retain all transformations in the accumulator
-        SparseArray<IconFontDescriptorWrapper> accumulator = new SparseArray<IconFontDescriptorWrapper>();
-        String result = recursivePrepareSpannableIndexes(new StringBuilder(text), iconFontDescriptors, accumulator, 0);
+        SparseArray<CustomTypefaceSpan> accumulator = new SparseArray<CustomTypefaceSpan>();
+        String result = recursivePrepareSpannableIndexes(context, text.toString(), new StringBuilder(text), iconFontDescriptors, accumulator, 0);
         SpannableString spannableString = SpannableString.valueOf(result);
 
         // Then apply spans at all positions
         int size = accumulator.size();
         for (int i = 0; i < size; i++) {
             int index = accumulator.keyAt(i);
-            IconFontDescriptorWrapper descriptor = accumulator.valueAt(i);
-            spannableString.setSpan(descriptor.getCustomTypefaceSpan(context), index, index + 1, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+            CustomTypefaceSpan descriptor = accumulator.valueAt(i);
+            spannableString.setSpan(descriptor, index, index + 1, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
         }
 
         return spannableString;
     }
 
-    private static String recursivePrepareSpannableIndexes(StringBuilder text,
+    private static String recursivePrepareSpannableIndexes(
+            Context context,
+            String fullText,
+            StringBuilder text,
             List<IconFontDescriptorWrapper> iconFontDescriptors,
-            SparseArray<IconFontDescriptorWrapper> accumulator,
+            SparseArray<CustomTypefaceSpan> accumulator,
             int start) {
 
-        // Try to find a {...} in the string and extract key from it
+        // Try to find a {...} in the string and extract expression from it
         int startIndex = text.indexOf("{", start);
         if (startIndex == -1) {
             return text.toString();
         }
         int endIndex = text.indexOf("}", startIndex) + 1;
-        String key = text.substring(startIndex + 1, endIndex - 1);
+        String expression = text.substring(startIndex + 1, endIndex - 1);
 
-        // Loop through the descriptors to find a match
+        // Split the expression and retrieve the icon key
+        String[] strokes = expression.split(" ");
+        String key = strokes[0];
+
+        // Loop through the descriptors to find a key match
         IconFontDescriptorWrapper iconFontDescriptor = null;
         Icon icon = null;
         for (int i = 0; i < iconFontDescriptors.size(); i++) {
@@ -55,16 +67,60 @@ public final class ParsingUtil {
             if (icon != null) break;
         }
 
-        // If no match, continue
+        // If no match, ignore and continue
         if (icon == null) {
-            return recursivePrepareSpannableIndexes(text, iconFontDescriptors, accumulator, endIndex);
+            return recursivePrepareSpannableIndexes(context, fullText, text, iconFontDescriptors, accumulator, endIndex);
+        }
+
+        // See if any more stroke within {} should be applied
+        float iconSizePx = -1;
+        int iconColor = -1;
+        for (int i = 1; i < strokes.length; i++) {
+            String stroke = strokes[i];
+
+            // Look for an icon size
+            if (stroke.matches("([0-9]*(\\.[0-9]*)?)dp")) {
+                iconSizePx = dpToPx(context, Float.valueOf(stroke.substring(0, stroke.length() - 2)));
+            } else if (stroke.matches("([0-9]*(\\.[0-9]*)?)sp")) {
+                iconSizePx = spToPx(context, Float.valueOf(stroke.substring(0, stroke.length() - 2)));
+            } else if (stroke.matches("([0-9]*)px")) {
+                iconSizePx = Integer.valueOf(stroke.substring(0, stroke.length() - 2));
+            } else if (stroke.matches("@dimen/(.*)")) {
+                iconSizePx = getPxFromDimen(context, stroke.substring(7));
+                if (iconSizePx < 0)
+                    throw new IllegalArgumentException("Unknown resource " + stroke + " in \"" + fullText + "\"");
+            } else {
+                throw new IllegalArgumentException("Unknown expression " + stroke + " in \"" + fullText + "\"");
+            }
         }
 
         // Get the typeface and set it
         text = text.replace(startIndex, endIndex, "" + icon.character());
-        accumulator.put(startIndex, iconFontDescriptor);
-        return recursivePrepareSpannableIndexes(text, iconFontDescriptors, accumulator, startIndex);
+        accumulator.put(startIndex, new CustomTypefaceSpan(
+                iconFontDescriptor.getIconFontDescriptor().ttfFileName(),
+                iconFontDescriptor.getTypeface(context),
+                iconSizePx, iconColor));
+        return recursivePrepareSpannableIndexes(context, fullText, text, iconFontDescriptors, accumulator, startIndex);
 
+    }
+
+    public static float getPxFromDimen(Context context, String resName) {
+        Resources resources = context.getResources();
+        int resId = resources.getIdentifier(
+                resName, "dimen",
+                context.getPackageName());
+        if (resId <= 0) return -1;
+        return resources.getDimension(resId);
+    }
+
+    public static float dpToPx(Context context, float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                context.getResources().getDisplayMetrics());
+    }
+
+    public static float spToPx(Context context, float sp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp,
+                context.getResources().getDisplayMetrics());
     }
 
 }
