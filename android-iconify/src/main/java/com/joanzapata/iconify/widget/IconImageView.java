8bcbfc9860d6166c08dfb09f977c24eb7e7e662c
Ibraheem Zaman <ibraheem@edx.org>
2015-12-06 21:50:11 +0500

    Add IconImageView and IconImageButton
    Although single icons can be rendered as text in text views, it requires
    setting a fixed size. Using a drawable allows it to automatically scale
    according to the size of the widget. Therefore, this commit introduces
    IconImageView and IconImageButton to enable using the drawable in XML.
    They also define custom attributes for the icon properties, which
    improves readability and extendibility. Furthermore, extending ImageView
    and ImageButton provides easy inoperability with other drawables (i.e
    when switching between XML drawable resources and IconDrawable).

---
+++ android-iconify/src/main/java/com/joanzapata/iconify/widget/IconImageView.java
@@ -0,0 +1,70 @@
+package com.joanzapata.iconify.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.joanzapata.iconify.Icon;
+import com.joanzapata.iconify.IconDrawable;
+import com.joanzapata.iconify.R;
+
+public class IconImageView extends ImageView {
+
+    private int color;
+
+    public IconImageView(Context context) {
+        super(context);
+    }
+
+    public IconImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public IconImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        final TypedArray a = context.obtainStyledAttributes(
+                attrs, R.styleable.IconImageView, defStyleAttr, 0);
+        color = a.getColor(R.styleable.IconImageView_iconColor, Color.BLACK);
+        String iconKey = a.getString(R.styleable.IconImageView_iconName);
+        if (iconKey != null) {
+            setImageDrawable(new IconDrawable(context, iconKey));
+        }
+        a.recycle();
+    }
+
+    public void setIcon(Icon icon) {
+        setImageDrawable(new IconDrawable(getContext(), icon));
+    }
+
+    public final Icon getIcon() {
+        Drawable drawable = getDrawable();
+        if (drawable instanceof IconDrawable) {
+            return ((IconDrawable) drawable).getIcon();
+        }
+        return null;
+    }
+
+    public void setIconColor(int color) {
+        this.color = color;
+        Drawable drawable = getDrawable();
+        if (drawable instanceof IconDrawable) {
+            ((IconDrawable) drawable).color(color);
+        }
+    }
+
+    public void setIconColorResource(int colorResId) {
+        setIconColor(getContext().getResources().getColor(colorResId));
+    }
+
+    @Override
+    public void setImageDrawable(Drawable drawable) {
+        if (drawable instanceof IconDrawable && drawable != getDrawable()) {
+            ((IconDrawable) drawable).color(color);
+        }
+        super.setImageDrawable(drawable);
+    }
+
+}
